0a8477ebc575ccd271bd3c0085ac32b9fd9c032d
das08 <takeda5955@gmail.com>
2020-02-10 01:51:21 +0900

    modify function

--- module/func.py
+++ module/func.py
@@ -2,118 +2,95 @@ import main as ap
 import json
 
 
+def prepareFlexMessage(token, color_theme, json_name, alt_text):
+    send = ap.Send(token)
+    f = open(f'./theme/{color_theme}/{json_name}.json', 'r', encoding='utf-8')
+    json_content = [json.load(f)]
+    send.send_result(json_content, alt_text, alt_text)
+
+
+def prepareOmikuji(token, color_theme, omikuji_type, alt_text):
+    send = ap.Send(token)
+    db = ap.DB()
+    prepare = ap.Prepare()
+
+    get_omikuji = db.get_omikuji(omikuji_type)
+
+    if get_omikuji[0] == "success":
+        getRakutanInfo = db.get_by_id(get_omikuji[1])
+        if getRakutanInfo[0] == 'success':
+            array = getRakutanInfo[1]
+            json_content = prepare.rakutan_detail(array, color_theme, omikuji_type)
+            send.send_result(json_content, alt_text, 'omikuji')
+        else:
+            send.send_text(getRakutanInfo[0])
+    else:
+        send.send_text("おみくじに失敗しました。もう一度引いてください。")
+
+
 def helps(token, lists):
-    send = ap.Send(token)
     color_theme = lists[2]
-    f = open(f'./theme/{color_theme}/help.json', 'r', encoding='utf-8')
-    json_content = [json.load(f)]
-    send.send_result(json_content, "ヘルプ", "ヘルプ")
-    return
+    prepareFlexMessage(token, color_theme, 'help', 'ヘルプ')
 
 
-def rakutan_hantei(token, lists):
-    send = ap.Send(token)
+def rakutanHantei(token, lists):
     color_theme = lists[2]
-    f = open(f'./theme/{color_theme}/hantei.json', 'r', encoding='utf-8')
-    json_content = [json.load(f)]
-    send.send_result(json_content, "判定詳細", "判定詳細")
-    return
+    prepareFlexMessage(token, color_theme, 'hantei', '判定詳細')
 
 
-def omikuji(token, lists):
+def selectTheme(token, lists):
+    color_theme = lists[2]
+    prepareFlexMessage(token, color_theme, 'theme_select', 'テーマ変更')
+
+
+def inquiry(token, lists):
+    prepareFlexMessage(token, 'etc', 'inquiry', 'お問い合わせ')
+
+
+def showVersion(token, lists):
+    prepareFlexMessage(token, 'etc', 'icon', '京大楽単bot')
+
+
+def normalOmikuji(token, lists):
+    color_theme = lists[2]
+    prepareOmikuji(token, color_theme, 'normal', '楽単おみくじ結果')
+
+
+def oniOmikuji(token, lists):
+    color_theme = lists[2]
+    prepareOmikuji(token, color_theme, 'oni', '鬼単おみくじ結果')
+
+
+def changeTheme(token, lists):
     send = ap.Send(token)
     db = ap.DB()
-    prepare = ap.Prepare()
-
-    fetch_omikuji = db.get_omikuji('normal')
-
-    if fetch_omikuji[0] == "success":
-        fetch_result = db.get_by_id(fetch_omikuji[1])
-        if fetch_result[0] == 'success':
-            array = fetch_result[1]
-            json_content = prepare.rakutan_detail(array, lists[2], "normal")
-            send.send_result(json_content, '楽単おみくじ結果', 'omikuji')
-        else:
-            send.send_text(fetch_result[0])
-    else:
-        send.send_text("おみくじに失敗しました。もう一度引いてください。")
-
-
-def onitan(token, lists):
-    send = ap.Send(token)
-    db = ap.DB()
-    prepare = ap.Prepare()
-
-    fetch_omikuji = db.get_omikuji('oni')
-
-    if fetch_omikuji[0] == "success":
-        fetch_result = db.get_by_id(fetch_omikuji[1])
-        if fetch_result[0] == 'success':
-            array = fetch_result[1]
-            json_content = prepare.rakutan_detail(array, lists[2], "oni")
-            send.send_result(json_content, '鬼単おみくじ結果', 'omikuji')
-        else:
-            send.send_text(fetch_result[0])
-    else:
-        send.send_text("おみくじに失敗しました。もう一度引いてください。")
-
-
-def select_theme(token, lists):
-    send = ap.Send(token)
-    color_theme = lists[2]
-    f = open(f'./theme/{color_theme}/theme_select.json', 'r', encoding='utf-8')
-    json_content = [json.load(f)]
-    send.send_result(json_content, "テーマ変更", "テーマ変更")
-
-
-def change_theme(token, lists):
-    send = ap.Send(token)
-    db = ap.DB()
-
     uid = lists[0]
     theme = lists[1][7:]
     db.update_db(uid, theme, 'theme')
     send.send_text("カラーテーマを変更しました！")
 
 
-def inquiry(token, lists):
-    send = ap.Send(token)
-    color_theme = lists[2]
-    f = open(f'./theme/etc/inquiry.json', 'r', encoding='utf-8')
-    json_content = [json.load(f)]
-    send.send_result(json_content, "お問い合わせ", "お問い合わせ")
-    return
-
-
-def my_uid(token, lists):
+def myUID(token, lists):
     send = ap.Send(token)
     uid = lists[0]
     send.send_text(f"Your uid: {uid}")
 
 
-def say_sorry(token, lists):
+def sorry(token, lists):
     send = ap.Send(token)
     send.send_text("申し訳ありません。現在この機能はご利用いただけません。")
 
 
-def say_sorry2(token, lists):
+def unavailable(token, lists):
     send = ap.Send(token)
     send.send_text("このテーマはゴールドユーザー限定です。")
 
 
-def show_version(token, lists):
-    send = ap.Send(token)
-    f = open(f'./theme/etc/icon.json', 'r', encoding='utf-8')
-    json_content = [json.load(f)]
-    send.send_result(json_content, "京大楽単bot", "京大楽単bot")
-    return
-
-
-def merge(token, lists):
+def checkKakomon(token, lists):
     ap.push_flex()
 
 
-def set_menu(token, lists):
+def setRichMenu(token, lists):
     send = ap.Send(token)
     uid = lists[0]
     menu = lists[1][5:]
@@ -122,8 +99,10 @@ def set_menu(token, lists):
     if menu == 'birdman':
         menu_id = "richmenu-fb2161dd36ae54baf01b17158eb22ca5"
         text += "\n@set:defaultで元に戻せます。"
+    elif menu == 'ad':
+        menu_id = "richmenu-fb2161dd36ae54baf01b17158eb22ca5"
+        text += "\n@set:defaultで元に戻せます。"
     else:
         menu_id = "richmenu-66a5b2117176dfd7d98055e2b6c85aed"
-
     ap.line_bot_api.link_rich_menu_to_user(uid, menu_id)
     send.send_text(text)
